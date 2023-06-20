dec6d2b2b99cf81554d7144efca398199245e480
TaoXinh123 <thuylinh04204@gmail.com>
2023-06-20 17:40:27 +0700

    project

---
+++ assets/js/slider.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".home-slide", {
+  // grabCursor:true, 
+  // loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
